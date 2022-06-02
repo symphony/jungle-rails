48907e762621923d1d866073d9b862ce2410e3f0
symphony <jciurc@gmail.com>
2022-06-02 14:41:24 -0600

    add auth env variables and authentication to admin controllers

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  #admin auth
+  http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all

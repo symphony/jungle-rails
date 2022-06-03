781de013b586052c3dd473b3656c350ac88d5d6e
symphony <jciurc@gmail.com>
2022-06-02 19:29:49 -0600

    fix product count in admin/categories. delete count helper

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(:id).all
   end
 
   def new
--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -1,5 +1,3 @@
 module Admin::CategoriesHelper
-  def product_count id
-    Product.where(:category_id == id).count
-  end
+
 end

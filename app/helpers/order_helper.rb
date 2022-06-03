f59a9680a653f7a1b199e0eeb33164527fd86961
symphony <jciurc@gmail.com>
2022-06-02 22:27:56 -0600

    add authentication to admin categories

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,7 +1,7 @@
 module OrderHelper
 
   def map_items
-    @items ||= @order.line_items.map {|item| {product: Product.find(item.product_id), quantity: item.quantity} }
+    @order.line_items.map {|item| {product: Product.find(item[:product_id]), quantity: item[:quantity]} }
   end
 
 end

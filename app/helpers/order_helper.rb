76e32d400e271178a56dbe9bbdea79ed9f4ef338
symphony <jciurc@gmail.com>
2022-06-02 22:06:07 -0600

    add new users table migration

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,7 +1,7 @@
 module OrderHelper
 
   def map_items
-    @order.line_items.map {|item| {product: Product.find(item[:product_id]), quantity: item[:quantity]} }
+    @items ||= @order.line_items.map {|item| {product: Product.find(item.product_id), quantity: item.quantity} }
   end
 
 end

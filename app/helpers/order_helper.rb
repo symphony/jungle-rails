e4b55998d97c3184de833947a823568d3169daf7
symphony <jciurc@gmail.com>
2022-06-03 22:21:55 -0600

    add margin top to main. remove redundant order total cost helper

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,9 +1,7 @@
 module OrderHelper
+
   def map_items
-    @items ||= @order.line_items.map {|item| {product: Product.find(item[:product_id]), quantity: item[:quantity]} } # todo make helper
+    @order.line_items.map {|item| {product: Product.find(item[:product_id]), quantity: item[:quantity]} }
   end
 
-  def get_total
-    @total ||= @items.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum # todo make helper
-  end
 end

422ac018a508de31ab27eeb152664b6c8d779fac
symphony <jciurc@gmail.com>
2022-06-07 00:13:10 -0600

    add rspec and net-smpt gems. create rspec test files

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new, :create] do
-      post :new
-      post :create
-    end
-  end
+    resources :categories, only: [:index, :new, :create]
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

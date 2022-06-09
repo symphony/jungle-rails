d65d133a77e7888bca04355f45408fc6ee450129
symphony <jciurc@gmail.com>
2022-06-08 23:04:17 -0600

    add homepage cypress test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate_with_credentials(params[:email], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to [:root]
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    render :new
   end
 
 end
---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,21 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000')
+  });
+
+  it('loads the homepage', () => {
+    cy.get("h2").contains('Where you can find any plants!')
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
